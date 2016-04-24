fa49a69f982ca92ca1d0d876d8b1c7d62d9db9c6
Rohan Nagar <rohan.nagar@utexas.edu>
2016-04-24 16:11:39 -0500

    Read in neighbor articles information

--- term_project/feature_extraction.py
+++ term_project/feature_extraction.py
@@ -1,5 +1,5 @@
 
-
+# Unigram/bigram feature extraction
 def unigram_overlap(article_title, term):
     words = term.split()
     overlap = 0
@@ -28,27 +28,35 @@ def bigram_overlap(article_title, article_abstract, term):
 
     return overlap
 
+# Reading in articles from file
+def add_articles_from_file(f, articles):
+    for line in f:
+        items = line.split('|')
+        pmid = items[0]
+        typ = items[1]
+
+        if pmid not in articles:
+            articles[pmid] = dict()
+
+        if typ == 't':
+            articles[pmid]["title"] = items[2]
+        elif typ == 'a':
+            articles[pmid]["abstract"] = items[2]
+        elif typ == 'm':
+            articles[pmid]["terms"] = items[2:]
+        else:
+            print("Unknown type of information")
+
 
 def initalize_articles():
     articles = dict()
 
+    # Original articles
     with open("paperdat/SMALL200/S200.TiAbMe", 'r') as f:
-        for line in f:
-            items = line.split('|')
-            pmid = items[0]
-            typ = items[1]
+        add_articles_from_file(f, articles)
 
-            if pmid not in articles:
-                articles[pmid] = dict()
-
-            if typ == 't':
-                articles[pmid]["title"] = items[2]
-            elif typ == 'a':
-                articles[pmid]["abstract"] = items[2]
-            elif typ == 'm':
-                articles[pmid]["terms"] = items[2:]
-            else:
-                print("Unknown type of information")
+    with open("paperdat/SMALL200/S200_50neighbors.TiAbMe", 'r') as f:
+        add_articles_from_file(f, articles)
 
     return articles
 
@@ -62,12 +70,6 @@ def add_neighbors_to_articles(articles):
 
             articles[pmid]["neighbors"].append(neighbor)
 
-def terms_for_article(pmid):
-    # Get the terms from the neighbors
-
-
-    # Get the terms from the citations
-    return None
 
 if __name__ == '__main__':
     articles = initalize_articles()
