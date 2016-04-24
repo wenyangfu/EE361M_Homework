dff3278cd585fefe71ce063ca2c8ce7a8a656833
Zuhair Parvez <zuhairparvez@gmail.com>
2016-04-24 16:02:02 -0500

    Added citations for NLM2007 and LARGE1000

--- term_project/scraper.py
+++ term_project/scraper.py
@@ -3,9 +3,10 @@ import sys
 from citation_grabber import write_citations
 
 with open('citations.TiAbMe', 'a+') as citations_file:
-    with open('paperdat/SMALL200/S200.pmids') as pubmed_ids_file:
-        for pubmed_id in pubmed_ids_file:
+    with open('paperdat/L1000/L1000.pmids') as pubmed_ids_file:
+        for num, pubmed_id in enumerate(pubmed_ids_file):
             to_fetch = pubmed_id.strip()
-            sys.stdout.write("%s\r" % to_fetch)
+            print(to_fetch)
+            sys.stdout.write("%03d %s\r" % (num, to_fetch))
             sys.stdout.flush()
             write_citations(citations_file, to_fetch)
